7abaa9d7e1f8939ea7e325626b9594371ce21c41
Felipe Cardozo <rene.cardozo.-nd@disney.com>
2023-08-15 11:44:50 -0500

    finish skills and remove scrolls

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,9 @@
 
     // custom scrollbar
 
+    /*
     $("html").niceScroll({styler:"fb",cursorcolor:"#ff9800", cursorwidth: '8', cursorborderradius: '10px', background: 'rgba(66, 79, 99, 0.6)', spacebarenabled:false, cursorborder: '0',  zindex: '1000'});
+
 
     $(".scrollbar1").niceScroll({styler:"fb",cursorcolor:"rgba(97, 100, 193, 0.78)", cursorwidth: '6', cursorborderradius: '0',autohidemode: 'false', background: '#F1F1F1', spacebarenabled:false, cursorborder: '0'});
 
@@ -13,6 +15,7 @@
     if ($('body').hasClass('scrollbar1-collapsed')) {
         $(".scrollbar1").getNiceScroll().hide();
     }
+    */
 
 })(jQuery);
 
